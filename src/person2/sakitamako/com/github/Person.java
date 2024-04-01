67a44d33c7cfd35d1df67144ede85a6352f19db5
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-01 21:43:52 +0900

    演習1追加

--- src/person2/sakitamako/com/github/Person.java
+++ src/person2/sakitamako/com/github/Person.java
@@ -1,5 +1,7 @@
 package person2.sakitamako.com.github;
 
 public class Person {
-
+	public String name = null;
+	public int age = 0;
 }
+
--- src/person2/sakitamako/com/github/Test.java
+++ src/person2/sakitamako/com/github/Test.java
@@ -1,5 +1,8 @@
 package person2.sakitamako.com.github;
 
 public class Test {
+	public static void main(String[] agrs) {
+		Person taro = new Person();
+	}
 
 }
